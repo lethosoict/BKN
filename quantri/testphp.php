67552bbc5a3a3876c42377bbd2ddebbc604f9fa7
lethosoict2016 <thomlew1012@gmail.com>
2018-11-15 17:54:22 +0700

    Install PHPEcel libraries
    1511

--- quantri/testphp.php
+++ quantri/testphp.php
@@ -34,6 +34,73 @@
 	echo $kq;
 	*/
 
+	require_once 'classes/PHPExcel/IOFactory.php';
+	
+	// Load existing file excel and get values from cells 
+	/*
+	if(isset($_FILES['excelFile']) && !empty($_FILES['excelFile']['tmp_name'])){
+			//var_dump($_FILES['excelFile']['tmp_name']);
+			$excelObject = PHPExcel_IOFactory::load($_FILES['excelFile']['tmp_name']);
+													 $getSheet = $excelObject->getActiveSheet()->toArray(null);
+					
+					echo '<pre>';
+					echo json_encode($getSheet);
+					echo var_dump($getSheet);  
+					
+			$workSheet = $excelObject->getSheet(0);
+			$lastRow = $workSheet->getHighestRow();
+			
+			
+			echo "<table>";
+						for ($row = 1; $row <= $lastRow; $row++){
+							echo "<tr><td>";
+							echo $workSheet->getCell('A'.$row)->getValue()." ||";
+							echo $workSheet->getCell('B'.$row)->getValue()." ||";
+							echo $workSheet->getCell('C'.$row)->getValue();
+							echo "</tr></td>";
+						}
+			echo "</table>";
+	 }
+	 */
+	
+	// Write value into excel file	 
+	$value = "Marco Van Bastel";
+	if(isset($_POST['submit'])){
+		$phpExcel = new PHPExcel();
+	$phpExcel->setActiveSheetIndex(0);
+	
+	// create header
+	$phpExcel->getActiveSheet()->SetCellValue("A3", $value);
+	
+	// save file excel
+	$objWrite =  new PHPExcel_Writer_Excel2007($phpExcel);
+	$objWrite->save("the_file.xlsx");
+	}
+	
+	
 	
 	
 ?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Database</title>
+<link rel="stylesheet" type="text/css" href="css/dangnhap.css" />
+</head>
+<body>
+	<!--
+	<form action="" method="post" enctype="multipart/form-data">
+			<input type="file" name="excelFile" />
+			<input type="submit" name="" value="submit" />
+			<button type="button" onclick="alert('Hello world!')"> Warning</button>
+		</form>
+	-->
+	
+	<form method="post" enctype="multipart/form-data">
+		<input type="submit" name="submit" value="cap nhat" />
+	</form>
+	
+</body>
+</html>
